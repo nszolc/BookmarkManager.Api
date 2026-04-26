28b38f80e65a6f16cd005631d7d63b8e7a1ac892
Natalia <nataliaszolc.kontakt@gmail.com>
2026-04-26 19:36:52 +0200

    feat: small corrections

--- BookmarkManager.Api/Models/Bookmark.cs
+++ BookmarkManager.Api/Models/Bookmark.cs
@@ -1,8 +1,11 @@
-﻿namespace BookmarkManager.Api.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookmarkManager.Api.Models
 {
     public class Bookmark
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
         public string? Description { get; set; }
